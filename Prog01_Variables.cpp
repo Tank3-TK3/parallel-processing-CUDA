26f401e72e4238e0caa5c738aad98931b7895d2e
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-06-26 13:03:52 -0500

    Update Prog01_Variables.cpp

--- Prog01_Variables.cpp
+++ Prog01_Variables.cpp
@@ -15,10 +15,14 @@ int main( int argc, char* argv[] )
 	int* bPtr = &b;
 	int* cPtr = &c;
 	float* fPtr = &f;
+
 	cout << "==================================================\n";
+
 	cout << "The sum of " << a << " + " << b << " = " << a + b << "\n";
 	cout << "The value of f is " << f << "\n";
+
 	cout << "==================================================\n";
+
 	cout << "The memory addresses of the variables are:\n";
 	cout << " A) a is: " << &a << "\n";
 	cout << " B) b is: " << &b << "\n";
@@ -28,13 +32,17 @@ int main( int argc, char* argv[] )
 	cout << " F) bPtr is: " << &bPtr << "\n";
 	cout << " G) cPtr is: " << &cPtr << "\n";
 	cout << " H) fPtr is: " << &fPtr << "\n";
+
 	cout << "==================================================\n";
+
 	cout << "The content of the pointer variables is:\n";
 	cout << " A) aPtr is: " << aPtr << "\n";
 	cout << " B) bPtr is: " << bPtr << "\n";
 	cout << " C) cPtr is: " << cPtr << "\n";
 	cout << " D) fPtr is: " << fPtr << "\n";
+
 	cout << "==================================================\n";
+
 	cout << "The original content of the variables referred by pointer are:\n";
 	cout << " A) aPtr is: " << *aPtr << "\n";
 	cout << " B) bPtr is: " << *bPtr << "\n";
@@ -44,13 +52,17 @@ int main( int argc, char* argv[] )
 	*bPtr = 777;
 	c = *bPtr + 13;
 	*cPtr = *cPtr + 25;
+
 	cout << "==================================================\n";
+
 	cout << "The actual content of the variables referred to by pointer are:\n";
 	cout << " A) aPtr is: " << *aPtr << "\n";
 	cout << " B) bPtr is: " << *bPtr << "\n";
 	cout << " C) cPtr is: " << *cPtr << "\n";
 	cout << " D) fPtr is: " << *fPtr << "\n";
+
 	cout << "==================================================\n";
+	
 	system( "pause" );
 	return 0;
 }
