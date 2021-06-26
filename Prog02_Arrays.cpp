9b5943c4f0cedf61f83272506d937ae3a3360463
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-06-26 13:05:13 -0500

    Update Prog02_Arrays.cpp

--- Prog02_Arrays.cpp
+++ Prog02_Arrays.cpp
@@ -13,6 +13,7 @@ int main( int argc , char* argv[] )
 	float array1[numRen];
 	float *array2;
 	float *array1Ptr;
+
 	array1Ptr = &array1[0];
 	array2 = ( float * ) malloc( numRen * sizeof( float ) );
 	srand( ( unsigned ) time( NULL ) );
@@ -21,30 +22,40 @@ int main( int argc , char* argv[] )
 		array1[i] = ( ( float ) rand() / ( float ) RAND_MAX ) * 100;
 		array2[i] = ( ( float ) rand() / ( float ) RAND_MAX ) * 100;
 	}
+
 	cout << "==================================================\n";
+
 	cout << "The memory addresses of the variables are:\n";
 	cout << "A ) array1 is: " << &array1[0] << "\n";
 	cout << "A') array1 is: " << &array1    << "\n";
 	cout << "B ) array2 is: " << &array2    << "\n";
+
 	cout << "==================================================\n";
+
 	cout << "The contents of the pointer variables are:\n";
 	cout << "A) array1Ptr is: " << array1Ptr << "\n";
 	cout << "B) array2 is: "    << array2    << "\n";
+
 	cout << "==================================================\n";
+
 	cout << "The memory addresses of the elements of the arrays are:\n";
 	for ( int i = 0 ; i < numRen ; ++i )
     {
 		cout << i + 1 << ") array1[" << i << "]: " << &array1[i];
 		cout << " -- array2[" << i << "]: " << &array2[i] << "\n";
 	}
+
 	cout << "==================================================\n";
+
 	cout << "The content of the arrays are:\n";
 	for ( int i = 0 ; i < numRen ; ++i )
 	{
 		cout << i + 1 << ") array1[" << i << "]: " << array1[i];
 		cout << " -- array2[" << i << "]: " << array2[i] << "\n";
 	}
+
 	cout << "==================================================\n";
+
 	cout << "The content of the arrays using index are:\n";
 	for ( int i = 0 ; i < numRen ; ++i )
 	{
@@ -53,7 +64,9 @@ int main( int argc , char* argv[] )
 		cout << i + 1 << ") array1[" << i << "]: " << array1[i];
 		cout << " -- array2[" << i << "]: " << array2[i] << "\n";
 	}
+
 	cout << "==================================================\n";
+
 	cout << "The content of the arrays using pointers are:\n";
 	for ( int i = 0 ; i < numRen ; ++i ) 
 	{
@@ -63,7 +76,9 @@ int main( int argc , char* argv[] )
 		cout << i + 1 << ") array1[" << i << "]: " << array1[i];
 		cout << " -- array2[" << i << "]: " << array2[i] << "\n";
 	}
+
 	cout << "==================================================\n";
+
     free( array2 );
     system( "pause" );
 	return 0;
