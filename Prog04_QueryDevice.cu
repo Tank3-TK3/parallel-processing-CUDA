d52cac0b992a3fea8d776ad41ec05a06183fa673
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-06-29 14:49:30 -0500

    Update Prog04_QueryDevice.cu

--- Prog04_QueryDevice.cu
+++ Prog04_QueryDevice.cu
@@ -12,65 +12,66 @@ string getDeviceArchitecture( cudaDeviceProp devProp )
 	string sign = "";
 	switch( devProp.major )
 	{
-	case 2:
-		sign = "Fermi";
-		break;
-	case 3:
-		sign = "Kepler";
-		break;
-	case 5:
-		sign = "Maxwell";
-		break;
-	case 6:
-		sign = "Pascal";
-		break;
-	case 7:
-		sign = "Volta or Turing";
-		break;
-	case 8:
-		sign = "Ampere";
-	default:
-		sign = "Unknown device type";
-		break;
+		case 2:
+			sign = "Fermi";
+			break;
+		case 3:
+			sign = "Kepler";
+			break;
+		case 5:
+			sign = "Maxwell";
+			break;
+		case 6:
+			sign = "Pascal";
+			break;
+		case 7:
+			sign = "Volta or Turing";
+			break;
+		case 8:
+			sign = "Ampere";
+			break;
+		default:
+			sign = "Unknown device type";
+			break;
 	}
 	return sign;
 }
 
 int getSPcores( cudaDeviceProp devProp )
 {
-    int cores = 0;
-    int mp = devProp.multiProcessorCount;
-    switch( devProp.major )
-    {
-        case 2:
-            if( devProp.minor == 1 ) cores = mp * 48;
-            else cores = mp * 32;
-            break;
-        case 3:
-            cores = mp * 192;
-            break;
-        case 5:
-            cores = mp * 128;
-            break;
-        case 6:
-            if( ( devProp.minor == 1 ) || ( devProp.minor == 2 ) ) cores = mp * 128;
-            else if( devProp.minor == 0 ) cores = mp * 64;
-            else cout << "Unknown device type\n";
-            break;
-        case 7:
-            if( ( devProp.minor == 0 ) || ( devProp.minor == 5 ) ) cores = mp * 64;
-            else cout << "Unknown device type\n";
-            break;
-        case 8:
-            if( devProp.minor == 0 ) cores = mp * 64;
-            else if( devProp.minor == 6 ) cores = mp * 128;
-            else cout << "Unknown device type\n";
-            break;
-        default:
-            cout << "Unknown device type\n"; 
-            break;
-    }
-    return cores;
+	int cores = 0;
+	int mp = devProp.multiProcessorCount;
+	switch( devProp.major )
+	{
+		case 2:
+			if( devProp.minor == 1 ) cores = mp * 48;
+			else cores = mp * 32;
+			break;
+		case 3:
+			cores = mp * 192;
+			break;
+		case 5:
+			cores = mp * 128;
+			break;
+		case 6:
+			if( ( devProp.minor == 1 ) || ( devProp.minor == 2 ) ) cores = mp * 128;
+			else if( devProp.minor == 0 ) cores = mp * 64;
+			else cout << "Unknown device type\n";
+			break;
+		case 7:
+			if( ( devProp.minor == 0 ) || ( devProp.minor == 5 ) ) cores = mp * 64;
+			else cout << "Unknown device type\n";
+			break;
+		case 8:
+			if( devProp.minor == 0 ) cores = mp * 64;
+			else if( devProp.minor == 6 ) cores = mp * 128;
+			else cout << "Unknown device type\n";
+			break;
+		default:
+			cout << "Unknown device type\n"; 
+			break;
+	}
+	return cores;
 }
 
 void printDevProp( int i )
@@ -90,14 +91,14 @@ void printDevProp( int i )
 	cout << " - Maximum pitch in bytes allowed by memory copies: " << devProp.memPitch << "\n";
 	cout << " - Maximum number of threads per block: " << devProp.maxThreadsPerBlock << "\n";
 	for( int i = 0 ; i < 3 ; ++i )
-        cout << " - Maximum dimension " << i << " of the grid: " << devProp.maxGridSize[i] << "\n";
+	cout << " - Maximum dimension " << i << " of the grid: " << devProp.maxGridSize[i] << "\n";
 	for ( int i = 0 ; i < 3 ; ++i )
-		cout << " - Maximum dimension " << i << " of the block: " << devProp.maxThreadsDim[i] << "\n";
+	cout << " - Maximum dimension " << i << " of the block: " << devProp.maxThreadsDim[i] << "\n";
 	cout << " - Clock frequency in kilohertz: " << devProp.clockRate << "\n";
 	cout << " - Constant memory available on device in bytes: " << devProp.totalConstMem << "\n";
-    cout << " - Number of asynchronous engines: " << devProp.asyncEngineCount << "\n";
-    cout << " - Specified whether there is a run time limit on kernels: " << devProp.kernelExecTimeoutEnabled << "\n";
-    cout << " - Alignment requirement for textures: " << devProp.textureAlignment << "\n";
+	cout << " - Number of asynchronous engines: " << devProp.asyncEngineCount << "\n";
+	cout << " - Specified whether there is a run time limit on kernels: " << devProp.kernelExecTimeoutEnabled << "\n";
+	cout << " - Alignment requirement for textures: " << devProp.textureAlignment << "\n";
 }
 
 int main( int argc, char* argv[] )
@@ -112,7 +113,6 @@ int main( int argc, char* argv[] )
 	for ( int i = 0 ; i < devCount ; ++i )
 	{
 		cout << "##################################################\n";
-		// Get device properties
 		cout << "+ CUDA device: " << i << "\n";
 		printDevProp( i );
 		cout << "##################################################\n\n";
