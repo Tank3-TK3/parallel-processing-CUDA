4fd9a7065bc4a879c1f658a4a8264c6848a31f9a
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-07-20 09:11:23 -0500

    Update Proj01_Pi.cu

--- Proj01_Pi.cu
+++ Proj01_Pi.cu
@@ -13,6 +13,19 @@
 
 const double iterations = 90000000; /*MAX: 94906264*/
 
+__host__ int printDevProp()
+{
+	cudaDeviceProp devProp;
+	cudaGetDeviceProperties(&devProp, 0);
+	printf("==================================================\n");
+	printf(" >>>>>>> PI Calculation with CPU and GPU <<<<<<<\n");
+	printf(" - Device Name: %s\n", devProp.name);
+	printf(" - Maximum number of threads per block: %d\n", devProp.maxThreadsPerBlock);
+	printf(" - Number of iterations: %.1lf\n", iterations);
+	printf("==================================================\n");
+	return devProp.maxThreadsPerBlock;
+}
+
 __host__ double piCPU()
 {
 	double sum = 0;
@@ -25,15 +38,15 @@ __host__ double piCPU()
 
 int main(int argc, char* argv[])
 {
-	printf("==================================================\n");
-	printf(" Calculation of PI on CPU and GPU.\n");
-	printf(" > Number of iterations: %.1lf\n", iterations);
-	printf("==================================================\n");
-	printf("\t\t<<<CPU>>>\n");
+	cudaSetDevice(0);
+
+	int maxHilos = printDevProp();
+
+	printf("\t\t<<<<< CPU >>>>>\n");
 	clock_t timer1 = clock();
-	printf("The value of PI in CPU is: %.8lf\n", piCPU()); /*MAX DEC: 51*/
+	printf(" - The value of PI in CPU is: %.8lf\n", piCPU()); /*MAX DEC: 51*/
 	timer1 = clock() - timer1;
-	printf("Total CPU time: %f ms.\n", ((((double)timer1) / CLOCKS_PER_SEC) * 1000));
+	printf(" - Total CPU time: %f ms.\n", ((((double)timer1) / CLOCKS_PER_SEC) * 1000));
 	printf("==================================================\n");
 
 	system("pause");
