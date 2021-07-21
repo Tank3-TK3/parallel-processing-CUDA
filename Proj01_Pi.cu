62db8786cfb11e65a9a5290c41acf314c0e6611e
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-07-20 22:58:49 -0500

    Update Proj01_Pi.cu

--- Proj01_Pi.cu
+++ Proj01_Pi.cu
@@ -26,7 +26,7 @@ __host__ int printDevProp()
 	return devProp.maxThreadsPerBlock;
 }
 
-__host__ void piCPU()
+__host__ double piCPU()
 {
 	printf("\t\t<<<<< CPU >>>>>\n");
 	double sum = 0;
@@ -36,17 +36,186 @@ __host__ void piCPU()
 		sum += (1 / (i * i));
 	}
 	timer1 = clock() - timer1;
-	printf(" - The value of PI in CPU is: %.8lf\n", sqrt(sum * 6)); /*MAX DEC: 51*/
-	printf(" - Total CPU time: %f ms.\n", ((((double)timer1) / CLOCKS_PER_SEC) * 1000));
+	double pi = sqrt(sum * 6);
+	printf(" - The value of PI in CPU is: %.8lf\n", pi); /*MAX DEC: 51*/
+	printf(" - Total CPU time: %f ms.\n", ( ( ( ( double ) timer1 ) / CLOCKS_PER_SEC ) * 1000.0 ) );
 	printf("==================================================\n");
+	return pi;
+}
+
+__host__ int printMenuOpt()
+{
+	int opt = ' ';
+	printf(" - Write the number of the case you want to run:\n");
+	printf("\t1) Case01 - 1 Block with m Thread.\n");
+	printf("\t2) Case02 - x Blocks with 1 Thread each.\n");
+	printf("\t3) Case03 - x Blocks with m Threads each.\n");
+	printf("\t4) Case04 - x * y Blocks with 1 Thread each.\n");
+	printf("\t5) Case05 - 1 Block with m * n Threads each.\n");
+	printf("\t6) Case06 - x * y Blocks with m * n Threads each.\n");
+	printf("> ");
+	scanf("%d", &opt);
+	return opt;
+}
+
+__global__ void sum01GPU01(double* arrayGPU, double iter)
+{
+	int tid = threadIdx.x;
+	double segmento = ((iter / 1024) * tid) + 1;
+	for (double i = segmento; i < (segmento + (iter / 1024)) - 1; ++i)
+	{
+		arrayGPU[tid] = arrayGPU[tid] + (1 / (i * i));
+	}
+}
+
+__global__ void sum02GPU01(double* arrayGPU, double* numpiGPUCPU)
+{
+	for (int i = 1; i < 1024; ++i)
+	{
+		arrayGPU[0] = arrayGPU[0] + arrayGPU[i];
+	}
+	numpiGPUCPU[0] = sqrt(arrayGPU[0] * 6);
+}
+
+__global__ void piGPU02()
+{
+
+}
+
+__global__ void piGPU03()
+{
+
+}
+
+__global__ void piGPU04()
+{
+
+}
+
+__global__ void piGPU05()
+{
+
+}
+
+__global__ void piGPU06()
+{
+
+}
+
+__host__ void printStats(clock_t timer, dim3 dimGrid, dim3 dimBlock)
+{
+	printf(" - Total Threads: %d\n",
+		dimGrid.x * dimGrid.y * dimGrid.z * dimBlock.x * dimBlock.y * dimBlock.z);
+	printf(" - Configuracion de ejecucion: \n");
+	printf("\t+ Grid [%d, %d, %d] Bloque [%d, %d, %d]\n",
+		dimGrid.x, dimGrid.y, dimGrid.z, dimBlock.x, dimBlock.y, dimBlock.z);
 }
 
 int main(int argc, char* argv[])
 {
+	clock_t timer;
+	dim3 dimGrid;
+	dim3 dimBlock;
+	cudaError_t cudaStatus;
+	double* numpiCPUGPU[1];
+	double* numpiGPUCPU;
+
+	cudaFree(0);
 	cudaSetDevice(0);
 
 	int maxHilos = printDevProp();
-	piCPU();
+
+	switch(printMenuOpt())
+	{
+	case 1: // Case01 - 1 Block with m Thread.
+		printf("==================================================\n");
+		printf("\t\t<<<<< GPU >>>>>\n");
+		double* arrayCPU[87891];
+		double* arrayGPU;
+		cudaMalloc((void**)&arrayGPU, (87891) * sizeof(double));
+		cudaMalloc((void**)&numpiGPUCPU, 1 * sizeof(double));
+		cudaMemset(arrayGPU, 0, (87891) * sizeof(double));
+		cudaMemcpy(arrayGPU, arrayCPU, iterations * sizeof(double), cudaMemcpyHostToDevice);
+		dimGrid = { 1 , 1 , 1 };
+		dimBlock = { 1024 , 1 , 1 };
+		timer = clock();
+		sum01GPU01<<<dimGrid, dimBlock>>>(arrayGPU, iterations);
+		cudaDeviceSynchronize();
+		dimBlock = { 1 , 1 , 1 };
+		cudaMemcpy(numpiGPUCPU, numpiCPUGPU, sizeof(double), cudaMemcpyHostToDevice);
+		sum02GPU01<<<dimGrid, dimBlock >>>(arrayGPU, numpiGPUCPU);
+		cudaDeviceSynchronize();
+		dimBlock = { 1024 , 1 , 1 };
+		timer = clock() - timer;
+		cudaMemcpy(numpiCPUGPU, numpiGPUCPU, sizeof(double), cudaMemcpyDeviceToHost);
+		printf(" - The value of PI in GPU is: %.8lf\n", numpiCPUGPU[0]); /*MAX DEC: 51*/
+		printf(" - Total GPU time: %f ms.\n", ((((double)timer) / CLOCKS_PER_SEC) * 1000.0));
+		printStats(timer, dimGrid, dimBlock);
+		printf("==================================================\n");
+		cudaFree(arrayGPU);
+		break;
+	case 2: // Case02 - x Blocks with 1 Thread each.
+		printf("==================================================\n");
+		printf("\t\t<<<<< GPU >>>>>\n");
+		dimGrid = { 1024 , 1 , 1 };
+		dimBlock = { 1 , 1 , 1 };
+		timer = clock();
+		piGPU02<<<dimGrid, dimBlock>>>();
+		timer = clock() - timer;
+		printStats(timer, dimGrid, dimBlock);
+		printf("==================================================\n");
+		break;
+	case 3: // Case03 - x Blocks with m Threads each.
+		printf("==================================================\n");
+		printf("\t\t<<<<< GPU >>>>>\n");
+		dimGrid = { 1024 , 1 , 1 };
+		dimBlock = { 1024 , 1 , 1 };
+		timer = clock();
+		piGPU03<<<dimGrid, dimBlock>>>();
+		timer = clock() - timer;
+		printStats(timer, dimGrid, dimBlock);
+		printf("==================================================\n");
+		break;
+	case 4: // Case04 - x * y Blocks with 1 Thread each.
+		printf("==================================================\n");
+		printf("\t\t<<<<< GPU >>>>>\n");
+		dimGrid = { 1024 , 1024 , 1 };
+		dimBlock = { 1 , 1 , 1 };
+		timer = clock();
+		piGPU04<<<dimGrid, dimBlock>>>();
+		timer = clock() - timer;
+		printStats(timer, dimGrid, dimBlock);
+		printf("==================================================\n");
+		break;
+	case 5: // Case05 - 1 Block with m * n Threads each.
+		printf("==================================================\n");
+		printf("\t\t<<<<< GPU >>>>>\n");
+		dimGrid = { 1 , 1 , 1 };
+		dimBlock = { 30 , 30 , 1 };
+		timer = clock();
+		piGPU05<<<dimGrid, dimBlock>>>();
+		timer = clock() - timer;
+		printStats(timer, dimGrid, dimBlock);
+		printf("==================================================\n");
+		break;
+	case 6: // Case06 - x * y Blocks with m * n Threads each.
+		printf("==================================================\n");
+		printf("\t\t<<<<< GPU >>>>>\n");
+		dimGrid = { 1024 , 1024 , 1 };
+		dimBlock = { 30 , 30 , 1 };
+		timer = clock();
+		piGPU06<<<dimGrid, dimBlock>>>();
+		timer = clock() - timer;
+		printStats(timer, dimGrid, dimBlock);
+		printf("==================================================\n");
+		break;
+	default:
+		printf(">>> INVALID OPTION <<<\n");
+		return 0;
+		break;
+	}
+
+	double numpiCPU = piCPU();
 
 	system("pause");
 	return 0;
